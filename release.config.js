1af381275091c15ded12c83f771be6488265a638
Sebastian Pekarek <mail@sebbo.net>
2021-07-14 22:15:26 +0200

    fix(CI): Fix TypeError: configuration.push is not a function

--- release.config.js
+++ release.config.js
@@ -10,42 +10,42 @@ const configuration = {
     'plugins': []
 };
 
-configuration.push(['@semantic-release/commit-analyzer', {
+configuration.plugins.push(['@semantic-release/commit-analyzer', {
     'releaseRules': [
         {'type': 'build', 'scope': 'deps', 'release': 'patch'},
         {'type': 'docs', 'release': 'patch'}
     ]
 }]);
 
-configuration.push('@semantic-release/release-notes-generator');
+configuration.plugins.push('@semantic-release/release-notes-generator');
 
-configuration.push(['@semantic-release/exec', {
+configuration.plugins.push(['@semantic-release/exec', {
     'prepareCmd': 'npm run build'
 }]);
 
-configuration.push('@semantic-release/changelog');
+configuration.plugins.push('@semantic-release/changelog');
 
-configuration.push('semantic-release-license');
+configuration.plugins.push('semantic-release-license');
 
-configuration.push(['@amanda-mitchell/semantic-release-npm-multiple', {
+configuration.plugins.push(['@amanda-mitchell/semantic-release-npm-multiple', {
     'registries': {
         'github': {},
         'public': {}
     }
 }]);
 
-configuration.push(['@semantic-release/github', {
+configuration.plugins.push(['@semantic-release/github', {
     'labels': false,
     'assignees': process.env.GH_OWNER
 }]);
 
-configuration.push(['@semantic-release/git', {
+configuration.plugins.push(['@semantic-release/git', {
     'assets': ['CHANGELOG.md', 'LICENSE'],
     'message': 'chore(release): :bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 }]);
 
 if(process.env.DOCKER_LOCAL_IMAGE_DH) {
-    configuration.push(['@eclass/semantic-release-docker', {
+    configuration.plugins.push(['@eclass/semantic-release-docker', {
         'baseImageName': process.env.DOCKER_LOCAL_IMAGE_DH,
         'registries': [
             {
@@ -59,7 +59,7 @@ if(process.env.DOCKER_LOCAL_IMAGE_DH) {
 }
 
 if(process.env.DOCKER_LOCAL_IMAGE_GH) {
-    configuration.push(['@eclass/semantic-release-docker', {
+    configuration.plugins.push(['@eclass/semantic-release-docker', {
         'baseImageName': 'ghcr.io/' + process.env.DOCKER_LOCAL_IMAGE_GH,
         'registries': [
             {
